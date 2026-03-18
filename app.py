f5b17107d4ce9b3985db313732f19c214bf6f621
Suyog Kumar Pokhrel <suyogpokhrel13@gmail.com>
2026-03-18 10:52:48 +0545

    add APIs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import ipl
+import records
 
 app = Flask(__name__)
 
@@ -12,5 +13,34 @@ def teams():
     teams = ipl.teamsAPI()
     return jsonify(teams)
 
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+
+    response = ipl.teamVteamAPI(team1, team2)
+
+    return jsonify(response)
+
+@app.route('/api/team-record')
+def team_record():
+    team_name = request.args.get('team')
+
+    response = records.teamAPI(team_name)
+    return response
+
+@app.route('/api/batting-record')
+def batting_record():
+    batsman_name = request.args.get('batsman')
+
+    response = records.batsmanAPI(batsman_name)
+    return response
+
+@app.route('/api/bowling-record')
+def bowling_record():
+    bowler_name = request.args.get('bowler')
+
+    response = records.bowlerAPI(bowler_name)
+    return response
 
 app.run(debug=True)
